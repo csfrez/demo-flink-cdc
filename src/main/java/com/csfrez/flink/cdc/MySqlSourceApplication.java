32edccef305cf7116557ec00f7c8504a9dd4fd6b
Frez <csfrez@163.com>
2021-11-19 23:08:54 +0800

    fink cdc 2.1

---
+++ src/main/java/com/csfrez/flink/cdc/MySqlSourceApplication.java
@@ -0,0 +1,32 @@
+package com.csfrez.flink.cdc;
+
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class MySqlSourceApplication {
+
+    public static void main(String[] args) throws Exception {
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                .hostname("115.159.34.194")
+                .port(3306)
+                .databaseList("mydb") // set captured database
+                .tableList("mydb.orders") // set captured table
+                .username("root")
+                .password("123456")
+                .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+                .build();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // enable checkpoint
+        env.enableCheckpointing(3000);
+
+        env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+           .setParallelism(2) // set 2 parallel source tasks
+           .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+
+        env.execute("Print MySQL Snapshot + Binlog");
+    }
+}
--- src/main/java/com/csfrez/flink/cdc/demo/MySqlSourceExample.java
+++ src/main/java/com/csfrez/flink/cdc/demo/MySqlSourceExample.java
@@ -1,16 +1,15 @@
 package com.csfrez.flink.cdc.demo;
 
-import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
-import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.csfrez.flink.cdc.bean.StatementBean;
-import com.csfrez.flink.cdc.debezium.FastjonDeserializationSchema;
-import com.csfrez.flink.cdc.debezium.JsonDebeziumDeserializationSchema;
 import com.csfrez.flink.cdc.function.BinlogFilterFunction;
 import com.csfrez.flink.cdc.function.BinlogFlatMapFunction;
 import com.csfrez.flink.cdc.sink.MySqlJdbcSink;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 /**
  * @author yangzhi
@@ -21,35 +20,32 @@ public class MySqlSourceExample {
 
     public static void main(String[] args) throws Exception {
         try{
-            SourceFunction<String> sourceFunction = MySQLSource.<String>builder()
+            MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
                     .hostname("115.159.34.194")
                     .port(3306)
                     .databaseList("mydb") // monitor all tables under inventory database
+                    .tableList("mydb.orders") // set captured table
                     .username("root")
                     .password("123456")
-                    .startupOptions(StartupOptions.latest())
-                    .deserializer(new FastjonDeserializationSchema()) // converts SourceRecord to String
+//                    .startupOptions(StartupOptions.latest())
+                    .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to String
 //                    .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to String
 //                    .deserializer(new StringDebeziumDeserializationSchema()) // converts SourceRecord to String
                     .build();
 
             StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-            DataStream<String> dataStream = env.addSource(sourceFunction);
-            DataStream<String> filterDataStream = dataStream.filter(new BinlogFilterFunction());
+            // enable checkpoint
+            env.enableCheckpointing(3000);
 
+            DataStream<String> dataStream = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source").setParallelism(1);
+//            dataStream.print().setParallelism(1);
+
+            DataStream<String> filterDataStream = dataStream.filter(new BinlogFilterFunction());
             DataStream<StatementBean> flatMapDataStream = filterDataStream.flatMap(new BinlogFlatMapFunction());
 
             flatMapDataStream.print();
             flatMapDataStream.addSink(new MySqlJdbcSink());
-
-//            SingleOutputStreamOperator<BinlogBean> singleOutputStreamOperator = filterDataStream.process(new BinlogProcessFunction());
-//            singleOutputStreamOperator.addSink()
-//            singleOutputStreamOperator.print();
-
-
-            //env.addSource(sourceFunction).print().setParallelism(1);
-            // use parallelism 1 for sink to keep message ordering
 
             env.execute();
         } catch (Exception e){
